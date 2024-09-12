bf46c2e766c236096a3a47dc11b6b1622474f8a9
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-13 00:15:00 +0200

    [UPD] Small cards done

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,13 +2,11 @@
 import { store } from "../store.js";
 import HeaderComponent from "./HeaderComponent.vue";
 import FooterComponent from "./FooterComponent.vue";
-import SmallCardComponent from "./SmallCardComponent.vue";
 export default {
   name: "MainComponent",
   components: {
     HeaderComponent,
     FooterComponent,
-    SmallCardComponent,
   },
 
   data() {
@@ -48,21 +46,39 @@ export default {
                 </button>
               </form>
             </div>
+            <div class="image-big">
+              <div class="text">
+                <h3>Come ordinare?</h3>
+              </div>
+              <div class="text">
+                <h2>E' semplicissimo!</h2>
+              </div>
+              <div class="small-cards-container">
+                <div class="small-card">
+                  <img src="../assets/img/small-card/burger.svg" alt="" />
+                  <h4>Scegli cosa vorresti mangiare</h4>
+                  <p>Vedrai in quali ristoranti lo puoi trovare</p>
+                </div>
+                <div class="small-card">
+                  <img
+                    src="../assets/img/small-card/shopping-cart.svg"
+                    alt=""
+                  />
+                  <h4>Metti ciò che desideri nel carrello</h4>
+                  <p>Puoi scegliere tra diversi metodi di pagamento</p>
+                </div>
+                <div class="small-card">
+                  <img src="../assets/img/small-card/dish-cap.svg" alt="" />
+                  <h4>Consegna a domicilio o ritiro al locale</h4>
+                  <p>Aspetta l'ordine in tutta tranquillità a casa</p>
+                </div>
+              </div>
+            </div>
           </div>
           <!-- END Search bar section -->
 
           <!-- Big image section -->
-          <div class="image-big">
-            <div>
-              <h3>Come ordinare?</h3>
-            </div>
-            <div>
-              <h2>E' semplicissimo!</h2>
-            </div>
-            <div>
-              <SmallCardComponent />
-            </div>
-          </div>
+
           <!-- END Big image section -->
         </div>
       </div>
