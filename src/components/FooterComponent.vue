c348149993ca4f03c0682afd19534db5d4e878cb
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-13 11:50:41 +0200

    [UPD] SVG images

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -16,6 +16,28 @@ export default {
 
 <template>
   <footer>
-    <h1>Welcome to the Footer Component</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-socials">
+          <div class="socials-container">
+            <ul>
+              <li>
+                <img src="../assets/img/footer/facebook.svg" alt="" />
+              </li>
+              <li>
+                <img src="../assets/img/footer/instagram.svg" alt="" />
+              </li>
+              <li>
+                <img src="../assets/img/footer/x-social-media.svg" alt="" />
+              </li>
+              <li>
+                <img src="../assets/img/footer/linkedin.svg" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-info"></div>
+      </div>
+    </div>
   </footer>
 </template>
