0b6063c9b81bb28919ffae079988b3fe796a2451
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-13 15:00:49 +0200

    [UPD] Footer done

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -49,12 +49,46 @@ export default {
           <div class="col-info">
             <div class="info-container d-flex justify-between">
               <div class="column-sx">
-                <h5>contatti</h5>
+                <h5>Contatti</h5>
+                <ul class="contacts-list">
+                  <li>
+                    <a href="">Chi siamo</a>
+                  </li>
+                  <li>
+                    <a href="">Contattaci</a>
+                  </li>
+                  <li>
+                    <a href="">Lavora con noi</a>
+                  </li>
+                  <li>
+                    <a href="">Diventa partner DeliveBoo</a>
+                  </li>
+                </ul>
               </div>
               <div class="column-dx">
-                <h5>supporto</h5>
+                <h5>Supporto</h5>
+                <ul class="support-list">
+                  <li>
+                    <a href="">FAQs</a>
+                  </li>
+                  <li>
+                    <a href="">Termini e condizioni</a>
+                  </li>
+                  <li>
+                    <a href="">Informativa cookie</a>
+                  </li>
+                  <li>
+                    <a href="">Informazioni legali</a>
+                  </li>
+                  <li>
+                    <a href="">Informativa sulla privacy</a>
+                  </li>
+                </ul>
               </div>
             </div>
+          </div>
+          <div class="copyright">
+            <p>&copy; 2024 DeliveBoo</p>
           </div>
         </div>
       </div>
