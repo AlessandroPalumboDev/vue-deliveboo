bee676722bb54a580bc2c14bb15268e3ed84d6f8
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-18 15:10:03 +0200

    [UPD] Fixes on the images and layout

--- src/pages/HomepageComponent.vue
+++ src/pages/HomepageComponent.vue
@@ -63,7 +63,7 @@ export default {
 <template>
   <main>
     <section>
-      <div class="container" v-cloak>
+      <div class="container">
         <div class="row">
           <!-- Jumbo section -->
           <div class="jumbo">
