55330012f88032590aa367dc6f402d6ffab6d232
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-09-18 17:10:25 +0200

    [UPD] file searchpage.vue renamed

--- src/router.js
+++ src/router.js
@@ -1,28 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomepageComponent from "./pages/HomepageComponent.vue";
-import SearchPage from "./pages/Searchpage.vue";  
-import RestaurantMenuPage from "./pages/RestaurantMenuPage.vue"; 
-
+import SearchPage from "./pages/SearchPage.vue";
+import RestaurantMenuPage from "./pages/RestaurantMenuPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",  // Homepage
+      path: "/", // Homepage
       name: "home",
       component: HomepageComponent,
     },
     {
-      path: "/search",  // Pagina di ricerca
+      path: "/search", // Pagina di ricerca
       name: "search",
       component: SearchPage,
-      props: (route) => ({ type: route.query.type }),  
+      props: (route) => ({ type: route.query.type }),
     },
     {
-      path: "/menu/",  
+      path: "/menu/",
       name: "restaurantMenu",
       component: RestaurantMenuPage,
-      props: true,  
+      props: true,
     },
   ],
 });
